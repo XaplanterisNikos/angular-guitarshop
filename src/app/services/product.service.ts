664e29bbbc3897830c3cbdfc6f815f71393feaf9
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-06-03 08:40:18 +0300

    define routes , menu links, category ID

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,7 +16,10 @@ export class ProductService {
  * Fetches the list of products from the API.
  * @returns {Observable<Product[]>} An observable containing the list of products.
  */
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // @TODO:  build URL based on category id
+
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
   );
