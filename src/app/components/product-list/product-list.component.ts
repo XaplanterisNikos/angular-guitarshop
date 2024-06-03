f714d7e858a827e83cb70c6c368f42478201555f
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-06-04 01:52:53 +0300

    search products

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,48 +1,67 @@
-import { Component } from '@angular/core';
-import { ProductService } from '../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+
 import { Product } from '../../common/product';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {
-[x: string]: any;
+export class ProductListComponent implements OnInit {
+  // [x: string]: any;
 
+  searchMode: boolean = false;
   products: Product[] = [];
   currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService,
-              private route: ActivatedRoute) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void{
-    this.route.paramMap.subscribe(() =>{
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
-    })
-    
+    });
   }
 
-  listProducts(){
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // search for the products
+    this.productService.searchProducts(theKeyword).subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  handleListProducts() {
     // ckeck id id parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if(hasCategoryId){
+    if (hasCategoryId) {
       // get the id param string . convert string to a number using the + symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-    }
-    else{
+    } else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
 
-      // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-    data => {
-      this.products = data;
-    }
-    )
+    // now get the products for the given category id
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,53 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, resolveForwardRef } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  
-
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-/**
- * Fetches the list of products from the API.
- * @returns {Observable<Product[]>} An observable containing the list of products.
- */
+  /**
+   * Fetches the list of products from the API.
+   * @returns {Observable<Product[]>} An observable containing the list of products.
+   */
   getProductList(theCategoryId: number): Observable<Product[]> {
-
     //  build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-  );
+    return this.getProducts(searchUrl);
   }
 
-  getProductCategories():Observable<ProductCategory[]> {
-    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
-      map(response => response._embedded.productCategory)
-    );
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    //  build URL based on keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 }
 
 /**
  * Interface for the API response.
  */
-interface GetResponseProducts{
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
-  }
+  };
 }
 
-interface GetResponseProductCategory{
+interface GetResponseProductCategory {
   _embedded: {
     productCategory: ProductCategory[];
-  }
+  };
 }
