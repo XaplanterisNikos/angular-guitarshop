73ac894cce03536b3fb9aa28fe0bcd857161571b
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-06-04 03:32:43 +0300

    detail-view

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,11 +8,19 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root',
 })
 export class ProductService {
+  
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) {}
+
+  getProduct(theProductId: number): Observable<Product> {
+    // build URL for product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 
   /**
    * Fetches the list of products from the API.
